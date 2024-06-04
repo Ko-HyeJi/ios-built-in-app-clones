b09cc6a0c2f317c03b6b80ead949e0359a4ac5c0
Ko-HyeJi <hy2ji.k5@gmail.com>
2024-06-04 17:54:02 +0900

    0604

--- compass/lib/main.dart
+++ compass/lib/main.dart
@@ -1,125 +1,271 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const int rotationAngle = 30;
+const double moving = 90;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+class App extends StatelessWidget {
+  App({super.key});
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final dataMap = <String, double>{
+    "1": moving,
+    "2": 360 - moving,
+  };
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return SafeArea(
+      child: MaterialApp(
+        home: Scaffold(
+          body: Column(
+            children: [
+              Flexible(
+                flex: 3,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    /// background
+                    Container(
+                      color: Colors.black,
+                    ),
+
+                    /// stick
+                    Column(
+                      children: [
+                        Container(
+                          width: 5,
+                          height: 70,
+                          color: Colors.white,
+                          margin: EdgeInsets.all(32),
+                        ),
+                      ],
+                    ),
+
+                    /// center circle
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey.withOpacity(0.15),
+                      ),
+                      margin: const EdgeInsets.all(150),
+                    ),
+
+                    /// rotation layer
+                    RotationTransition(
+                      turns: AlwaysStoppedAnimation(rotationAngle / 360),
+                      child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 270,
+                            width: 270,
+                            child: DottedBorder(
+                              borderType: BorderType.Circle,
+                              strokeWidth: 20,
+                              color: Colors.grey.withOpacity(0.5),
+                              dashPattern: [2, 3],
+                              child: ClipRRect(),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 270,
+                            width: 270,
+                            child: DottedBorder(
+                              borderType: BorderType.Circle,
+                              strokeWidth: 20,
+                              color: Colors.white,
+                              dashPattern: [3, 50],
+                              child: ClipRRect(),
+                            ),
+                          ),
+                          RotationTransition(
+                              turns: AlwaysStoppedAnimation(
+                                  -rotationAngle * 4 / 360),
+                              child: Icon(
+                                Icons.arrow_right,
+                                color: Colors.red,
+                                size: 50,
+                              )),
+                        ],
+                      ),
+                    ),
+
+                    /// pie chart
+                    SizedBox(
+                      width: 225,
+                      child: PieChart(
+                        dataMap: dataMap,
+                        chartType: ChartType.ring,
+                        animationDuration: Duration(seconds: 0),
+                        initialAngleInDegree: -90,
+                        colorList: [Colors.red, Colors.black],
+                        ringStrokeWidth: 35,
+                        chartValuesOptions:
+                            ChartValuesOptions(showChartValues: false),
+                        legendOptions: LegendOptions(showLegends: false),
+                      ),
+                    ),
+
+                    /// 동,서,남,북
+
+                    const RotationTransition(
+                      turns: AlwaysStoppedAnimation(rotationAngle / 360),
+                      child: Stack(
+                        children: [
+                          SizedBox(
+                            width: 230,
+                            height: 230,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                RotationTransition(
+                                  turns: AlwaysStoppedAnimation(
+                                      -rotationAngle / 360),
+                                  child: Text(
+                                    '북',
+                                    style: TextStyle(
+                                        fontSize: 30, color: Colors.white),
+                                  ),
+                                ),
+                                RotationTransition(
+                                  turns: AlwaysStoppedAnimation(
+                                      -rotationAngle / 360),
+                                  child: Text(
+                                    '남',
+                                    style: TextStyle(
+                                        fontSize: 30, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 230,
+                            height: 230,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                RotationTransition(
+                                  turns: AlwaysStoppedAnimation(
+                                      -rotationAngle / 360),
+                                  child: Text(
+                                    '서',
+                                    style: TextStyle(
+                                        fontSize: 30, color: Colors.white),
+                                  ),
+                                ),
+                                RotationTransition(
+                                  turns: AlwaysStoppedAnimation(
+                                      -rotationAngle / 360),
+                                  child: Text(
+                                    '동',
+                                    style: TextStyle(
+                                        fontSize: 30, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    /// small cross
+                    Container(
+                      width: 1.5,
+                      height: 20,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      width: 20,
+                      height: 1.5,
+                      color: Colors.grey,
+                    ),
+
+                    /// large cross
+                    Container(
+                      width: 0.8,
+                      height: 170,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      width: 170,
+                      height: 0.8,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      color: Colors.black,
+                    ),
+                    const Text(
+                      '240° 남서',
+                      style: TextStyle(
+                        fontSize: 75,
+                        fontWeight: FontWeight.w200,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      color: Colors.black,
+                    ),
+                    const Column(
+                      children: [
+                        Text(
+                          '37°46\'21\"북 122°29\'32\"서',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'San Francisco, CA',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '고도 39.6m',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
